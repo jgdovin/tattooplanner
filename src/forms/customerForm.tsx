d0de828b62a68f0f6a15205f80827b7e58ba214e
Josh Gdovin <joshgdovin@gmail.com>
2024-08-04 10:59:57 -0600

    better customer validation

--- src/forms/customerForm.tsx
+++ src/forms/customerForm.tsx
@@ -42,7 +42,7 @@ const InputField = ({ name, form, label, textArea }: any) => (
 );
 
 export const formSchema = z.object({
-  name: z.string().min(3),
+  name: z.string().min(3, { message: "Name must be at least 3 characters" }),
   email: z.string().email(),
   phone: zPhone,
   notes: z.string(),
