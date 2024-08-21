0ee7e96702754ed0998becc61b4d6e5961674b0d
Josh Gdovin <joshgdovin@gmail.com>
2024-08-21 14:13:45 -0600

    fix typo

--- src/app/dashboard/bookings/client.tsx
+++ src/app/dashboard/bookings/client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import FullCalendar from "@fullCalendar/react";
-import timeGridPlugin from "@fullCalendar/timegrid";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import { useEffect, useState } from "react";
 
 export default function Client() {
